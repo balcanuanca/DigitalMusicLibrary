4f08b1826c49429df9da71e62519a7fd385f7608
balcanuanca <balcanu.anca@gmail.com>
2024-07-09 13:55:42 +0300

    initial commit

---
+++ src/main/java/com/AncaBalcanu/DigitalMusicLibrary/DigitalMusicLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.AncaBalcanu.DigitalMusicLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalMusicLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalMusicLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AncaBalcanu/DigitalMusicLibrary/DigitalMusicLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AncaBalcanu.DigitalMusicLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalMusicLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
