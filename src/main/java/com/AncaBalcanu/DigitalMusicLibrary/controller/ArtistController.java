7ed2dab61b91239913ea3e8e684b58bb76b296a6
balcanuanca <balcanu.anca@gmail.com>
2024-07-11 14:36:44 +0300

    Create from CRUD for Artist

--- src/main/java/com/AncaBalcanu/DigitalMusicLibrary/controller/ArtistController.java
+++ src/main/java/com/AncaBalcanu/DigitalMusicLibrary/controller/ArtistController.java
@@ -5,6 +5,7 @@ import com.AncaBalcanu.DigitalMusicLibrary.service.ArtistService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
 
@@ -30,6 +31,18 @@ public class ArtistController {
         Artist foundArtist = artistService.findById(id).get();
         model.addAttribute("foundArtist", foundArtist);
         return "showArtistDetails";
+    }
+
+    @GetMapping("/new")
+    public String showAddArtistForm(Model model)
+    {
+        model.addAttribute("artist", new Artist());
+        return "artistForm";
+    }
+    @PostMapping("/new")
+    public ModelAndView addArtist(Artist artist){
+        artistService.save(artist);
+        return new ModelAndView("redirect:http://localhost:8080/artists");
     }
 
 
